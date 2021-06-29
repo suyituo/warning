727a8055ce52f5eca90fddc81175c7cded97e0a2
suyituo <syt_dm@163.com>
2021-06-29 10:43:24 +0800

    新增： 1、事件类型管理（增加事件、修改事件、删除事件） 2、事件管理（事件记录查询） 3、告警组管理（告警组添加、修改、删除） Signed-off-by: suyituo <syt_dm@163.com>

---
+++ files/event/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m,n,o],p,_(q,r,s,t,u,v,w,_(),x,_(y,z,A,B,C,_(D,E,F,G),H,null,I,B,J,B,K,L,M,null,N,O,P,Q,R,S,T,O),U,_(V,_()),W,_(),X,_(Y,[])),Z,_(),ba,_());}; 
+var b="url",c="event.html",d="generationDate",e=new Date(1624934497219.64),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="tabletwo",o="tablethree",p="page",q="packageId",r="6b932480f2bb43b388881ac2973243ae",s="type",t="Axure:Page",u="name",v="event",w="notes",x="style",y="baseStyle",z="627587b6038d43cca051c114ac41ad32",A="pageAlignment",B="near",C="fill",D="fillType",E="solid",F="color",G=0xFFFFFFFF,H="image",I="imageHorizontalAlignment",J="imageVerticalAlignment",K="imageRepeat",L="auto",M="favicon",N="sketchFactor",O="0",P="colorStyle",Q="appliedColor",R="fontName",S="Applied Font",T="borderWidth",U="adaptiveStyles",V="a",W="interactionMap",X="diagram",Y="objects",Z="masters",ba="objectPaths";
+return _creator();
+})());
